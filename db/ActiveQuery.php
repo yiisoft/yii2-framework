f38772b6c18defcc29d0a7a76445ad7162a8a2e4
Stephen Seliuk <stephen@seliuk.com>
2014-08-01 23:58:29 +0300

    Added property $alias for set the alias for the $modelClass table name.

--- db/ActiveQuery.php
+++ db/ActiveQuery.php
@@ -95,6 +95,11 @@ class ActiveQuery extends Query implements ActiveQueryInterface
      */
     public $joinWith;
 
+    /**
+     * @var string Alias for the [[ActiveQueryTrait::$modelClass]] table name
+     */
+    public $alias;
+
 
     /**
      * Constructor.
@@ -144,7 +149,7 @@ class ActiveQuery extends Query implements ActiveQueryInterface
             /* @var $modelClass ActiveRecord */
             $modelClass = $this->modelClass;
             $tableName = $modelClass::tableName();
-            $this->from = [$tableName];
+            $this->from = empty($this->alias)?[$tableName]:[$this->alias => $tableName];
         }
 
         if (empty($this->select) && !empty($this->join)) {
@@ -718,4 +723,5 @@ class ActiveQuery extends Query implements ActiveQueryInterface
 
         return $this;
     }
+
 }
